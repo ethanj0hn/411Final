8fd3ab29d4ab78eced99dfe1a69ce214e899acf8
Anirudh Kashyap <kashyap5@illinois.edu>
2020-11-10 15:35:06 -0800

    adding datapath file, top level signals and IF_ID

--- mp4/hdl/IF_ID.sv
+++ mp4/hdl/IF_ID.sv
@@ -3,9 +3,50 @@ IF/ID stage in the pipeline. Includes following datapath elements:
 PC, PCMUX IF/ID buffer
 IF/ID buffer stores the PC to be passed down the pipeline, Instruction from inst memory.
 Inputs - clk, reset, PC + offset for branch, read val from instruction memory.
-Outputs - imm vals, instruction, PC value
+Outputs - imm vals, instruction, PC value, instruction read signal
 */
 
 module IF_ID(
+    input logic clk,
+    input logic reset,
+    input logic br_en, // from ex stage cmp
+    input logic br_cw, // from control word on whether there is a branch trying to happen
+    input logic j_cw, // jump control word
+    input logic [31:0] br_PC, // if branch to be taken, PC + offset
+    output logic [31:0] inst_addr, // instr read from memory
+    output logic inst_read // read signal for instruction memory
+);
 
+// interal logic for PC
+//
+logic PC_MUX_sel, take_branch;
+logic [31:0] PC_in;
+
+// take branch logic high when there is a branch cw in execute stage and br_en high
+// PC MUX sel takes alu output when take_branch OR jump control word
+//
+assign take_branch = br_en & br_cw;
+assign PC_MUX_sel = take_branch | take_branch;
+
+
+always_comb
+begin
+    inst_read = 1'b1; // always read
+
+    // PCMUX logic
+    //
+    if(PC_MUX_sel)
+        PC_in = br_PC;
+    else
+        PC_in = inst_addr + 32'h4;
+end
+
+// PC in the IF stage that send address to memory
+//
+pc_register PC(
+    .clk(clk),
+    .rst(reset),
+    .load(1'b1), // for now, we are always loading every cycle change when stalling pipeline required
+    .in(PC_in),
+    .out(inst_addr)
 );
--- mp4/hdl/mp4.sv
+++ mp4/hdl/mp4.sv
@@ -1,3 +1,19 @@
-module mp4();
+module mp4(
+    input logic clk,
+    input logic reset,
+    input logic data_resp, // response from data, instruction memory
+    input logic inst_resp,
+    input logic [31:0] inst_rdata, // instruction, data read port
+    input logic [31:0] data_rdata,
+    output logic inst_read, // instruction port address, read signal
+    output logic [31:0] inst_addr,
+    output logic data_read, // data read write signals
+    output logic data_write, 
+    output logic [3:0] data_mbe, // mem_byte_enable, signals data port address
+    output logic [31:0] data_addr, 
+    output logic [31:0] data_wdata    
+);
+
+pipeline_datapath datapath(.*);
 
 endmodule : mp4
