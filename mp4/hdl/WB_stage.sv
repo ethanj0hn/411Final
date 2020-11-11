4fad524e7341ee8df5417f8b68d9104d21f19cf0
Ethan John <ethanj2@illinois.edu>
2020-11-10 23:28:11 -0600

    changes to mem_stage

--- mp4/hdl/WB_stage.sv
+++ mp4/hdl/WB_stage.sv
@@ -4,36 +4,65 @@ module WB_stage(
 
     //inputs
 
-    input [31:0] alu_out, //from EX stage
-    input [31:0] read_data, //from Data Memory
-    input alu_read_sel,
+    input 
+    input logic [2:0] funct3, 
 
-    
+    input [1:0] mem_address_last_two_bits,
     input rv32i_control_word ctrl,
 
-
-    output logic wb_pc,
     //outputs to ID_stage
     output logic [4:0] rd_wb,
     output logic load_regfile_wb,
     output logic [31:0] regfilemux_out_wb
 )
 
-logic [31:0] mux_out;
 
 always_comb begin : MUXES
 
-    unique case(alu_read_sel):
-        1'b0: mux_out = alu_out;
-        1'b1: mux_out = read_data;
-        default: mux_out = alu_out;
-    endcase
-
-    unique case(ctrl.regfilemux_sel):
-        regfilemux::alu_out: regfilemux_out_wb = mux_out;
+    unique case(regfilemux_sel)
+        regfilemux::alu_out: regfilemux_out_wb = alu_out;
         regfilemux::br_en: regfilemux_out_wb = br_en;
         regfilemux::u_imm: regfilemux_out_wb = u_imm;
-        default: regfilemux_out_wb = mux_out;
-    endcase
+        regfilemux::lw: regfilemux_out_wb = mdrreg_out;
+        regfilemux::pc_plus4: regfilemux_out_wb = pc_out + 4;
+        regfilemux::lb:
+        begin
+            unique case(rmask)
+                4'b0001: regfilemux_out_wb = { {24{mdrreg_out[7]}}, mdrreg_out[7:0]};
+                4'b0010: regfilemux_out_wb = { {24{mdrreg_out[15]}}, mdrreg_out[15:8]};
+                4'b0100: regfilemux_out_wb = { {24{mdrreg_out[23]}}, mdrreg_out[23:16]};
+                4'b1000: regfilemux_out_wb = { {24{mdrreg_out[31]}}, mdrreg_out[31:24]};
+                default: regfilemux_out_wb = { {24{mdrreg_out[23]}}, mdrreg_out[7:0]};
+            endcase
+        end
+        regfilemux::lbu:
+        begin
+            unique case(rmask)
+                4'b0001: regfilemux_out_wb =  {24'b0, mdrreg_out[7:0]};
+                4'b0010: regfilemux_out_wb =  {24'b0, mdrreg_out[15:8]};
+                4'b0100: regfilemux_out_wb =  {24'b0, mdrreg_out[23:16]};
+                4'b1000: regfilemux_out_wb =  {24'b0, mdrreg_out[31:24]};
+                default: regfilemux_out_wb =  {24'b0, mdrreg_out[7:0]};
+            endcase
+        end
+        regfilemux::lhu:
+        begin
+            unique case(rmask)
+                4'b0011: regfilemux_out_wb =  {16'b0, mdrreg_out[15:0]};
+                4'b1100: regfilemux_out_wb =  {16'b0, mdrreg_out[31:16]};
+                default: regfilemux_out_wb =  {16'b0, mdrreg_out[15:0]};
+            endcase
+        end
+        regfilemux::lh:
+        begin
+            unique case(rmask)
+                4'b0011: regfilemux_out_wb = { {16{mdrreg_out[15]}}, mdrreg_out[15:0]};
+                4'b1100: regfilemux_out_wb = { {16{mdrreg_out[31]}}, mdrreg_out[31:16]};
+                default: regfilemux_out_wb = { {16{mdrreg_out[15]}}, mdrreg_out[15:0]};
+            endcase
+        end
+        
 
+        default: regfilemux_out = 0;
+    endcase
 end
