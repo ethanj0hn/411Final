1b0ca126cd8c17e4d1b06eaa7241d6304b583593
Ethan John <ethanj2@illinois.edu>
2020-11-11 02:02:10 -0600

    other tests

---
+++ mp4/testcode/imm+jal_alui.s
@@ -0,0 +1,79 @@
+.align 4
+.section .text
+.globl _start
+_start:
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    jal x1, ld_reg
+    nop
+    nop
+ld_reg:
+    lw x5, testval
+    jalr x2, 3(x1)    
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    addi x1, x0, 5 
+    addi x7, x0, 7
+    addi x9, x0, 9
+    addi x15, x0, -5
+    addi x16, x0, 1
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    slti x1, x0, 1
+    andi x7, x0, 1
+    xori x9, x0, 0
+    ori x15, x0, 1
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    auipc x7, 8         # X7 <= PC + 8
+    lui  x2, 2       # X2 <= 2
+    lui  x3, 8     # X3 <= 8
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+HALT:
+    beq x0, x0, HALT
+
+
+
+
+
+
+.section .rodata
+.balign 256
+testval:    .word 0x0000B00B
